4dc99b2e3164ab0434aa0bb03f89890605288276
danyadhi <danyadhi4149@gmail.com>
2024-12-04 09:01:05 +0700

    feat(auth-middleware): Update how to use middleware

--- internal/routes/user.go
+++ internal/routes/user.go
@@ -10,15 +10,18 @@ import (
 )
 
 func SetupUserRouter(router *mux.Router, database *sql.DB) *mux.Router {
+	userRouter := router.NewRoute().Subrouter()
+	userRouter.Use(utils.AuthMiddleware)
+
 	usersRepo := users.NewRepository(database)
 	usersService := users.NewService(usersRepo)
 	userHandler := users.NewHandler(usersService)
 
-	router.Handle("/users", utils.AuthMiddleware(http.HandlerFunc(userHandler.GetAllUsersHandler))).Methods(http.MethodGet)
-	router.Handle("/users/{id}", utils.AuthMiddleware(http.HandlerFunc(userHandler.GetOneUsersHandler))).Methods(http.MethodGet)
-	router.Handle("/users", utils.AuthMiddleware(http.HandlerFunc(userHandler.CreateUsersHandler))).Methods(http.MethodPost)
-	router.Handle("/users/{id}", utils.AuthMiddleware(http.HandlerFunc(userHandler.UpdateUsersHandler))).Methods(http.MethodPut)
-	router.Handle("/users/{id}", utils.AuthMiddleware(http.HandlerFunc(userHandler.DeleteusersHandler))).Methods(http.MethodDelete)
+	userRouter.HandleFunc("/users", userHandler.GetAllUsersHandler).Methods(http.MethodGet)
+	userRouter.HandleFunc("/users/{id}", userHandler.GetOneUsersHandler).Methods(http.MethodGet)
+	userRouter.HandleFunc("/users", userHandler.CreateUsersHandler).Methods(http.MethodPost)
+	userRouter.HandleFunc("/users/{id}", userHandler.UpdateUsersHandler).Methods(http.MethodPut)
+	userRouter.HandleFunc("/users/{id}", userHandler.DeleteusersHandler).Methods(http.MethodDelete)
 
 	return router
 }
